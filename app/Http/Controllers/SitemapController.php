517bbbcb9afb2a0866ac9b9b00981cc0924ae36f
LUKE <you@example.com>
2018-10-24 11:16:26 +0800

    Asd

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Content;
+use App\Yuming;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -9,29 +11,38 @@ class SitemapController extends Controller
 {
     //
 
+    function __construct()
+    {
+        $this->host = $_SERVER['HTTP_HOST'];
+        if (strpos($this->host,'www.') !== false){
+            $this->domain = str_after($this->host,'.');
+        }else{
+            $this->domain = $this->host;
+        }
+        $file = @fopen(public_path('outlink/url.txt'),'r');
+        $i = 0;
+        $this->outlink = array();
+        while (!feof($file)){
+            $line = fgets($file);
+            if($line){
+                $this->outlink[$i] = $line;
+                $i++;
+            }
+        }
 
+    }
 
 
 
     function index(){
-        $count = DB::select('select count(1) from content');
-        $a = self::object_to_array($count[0]);
-        $count = $a['count(1)'];
-        $id = rand(0,$count);
-        $content = Content::where(['id' > $id])->limit(250)->get();
-        return view('sitemap')->with(['content'=>$content]);
+
+        $host = Yuming::where(['host'=>$this->domain])->first();
+        $content = Content::orderby('id','desc')->get()->toArray();
+        return view('sitemap')->with(['content'=>$content,'host'=>$host]);
     }
 
 
 
-    function object_to_array($obj){
-        $_arr=is_object($obj)?get_object_vars($obj):$obj;
-        foreach($_arr as $key=>$val){
-            $val=(is_array($val))||is_object($val)?self::object_to_array($val):$val;
-            $arr[$key]=$val;
-        }
-        return $arr;
-    }
 
 
 }
